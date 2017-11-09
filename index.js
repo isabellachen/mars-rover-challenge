fd023504acfb3592172a10e9a9d346b225bc15b5
Isabella Chen <miss.izzyc@gmail.com>
2017-11-09 19:48:50 +0100

    FEAT: added logic for the mission toread of directions to the rover

--- index.js
+++ index.js
@@ -58,6 +58,23 @@ const mission = (input) => {
     //loop through the direction string, moving the rover
     const currentDirection = directions[j];
     const currentRover = rovers[j];
+    for (let cmd=0; cmd<currentDirection.length; cmd++) {
+      const orientation = currentRover.orientation;
+      if (currentDirection[cmd] === 'L') {
+        //turn the rover left from the cardinal it is currently pointing at
+        currentRover.orientation = cardinals[orientation]['L'];
+      }
+      if (currentDirection[cmd] === 'R') {
+        //turn the rover right from the cardinal it is currently pointing at
+        currentRover.orientation = cardinals[orientation]['R'];
+      }
+      if (currentDirection[cmd] === 'M') {
+        //e.g. if rover orientation is 'N', the rover's position y property should be y+1
+        const x = currentRover.position.x;
+        const y = currentRover.position.y;
+        currentRover.position = cardinals[orientation]['move'](x, y);
+      }
+    }
   }
   return rovers;
 };
