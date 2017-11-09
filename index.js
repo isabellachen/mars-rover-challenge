96eb019dc0291960164999796785c3a6d36c287c
Isabella Chen <miss.izzyc@gmail.com>
2017-11-09 19:08:26 +0100

    FEAT: File structure set up

---
+++ index.js
@@ -0,0 +1,17 @@
+const cardinals = {
+
+};
+
+const createRover = (startPos) => {
+
+};
+
+const mission = (input) => {
+
+};
+
+const input = '5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM';
+
+const result = mission(input);
+
+console.log(result);
