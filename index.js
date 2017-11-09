b34f10c8d02dcbbf048c4086443a337eecd70004
Isabella Chen <miss.izzyc@gmail.com>
2017-11-09 19:30:10 +0100

    FEAT: factory func creating rovers

--- index.js
+++ index.js
@@ -30,16 +30,27 @@ const cardinals = {
 };
 
 const createRover = (startPos) => {
-
+  const x = parseInt(startPos.split(' ')[0]);
+  const y = parseInt(startPos.split(' ')[1]);
+  const orientation = startPos.split(' ')[2];
+  const rover = {
+    position: {
+      x: x,
+      y: y,
+    },
+    orientation: orientation,
+  };
+  return rover;
 };
 
 const mission = (input) => {
   input = input.split('\n');
   const plateauBounds = input[0];
   const rovers = [];
-
+  const directions = [];
   for (let i=1; i<input.length; i=i+2) {
     rovers.push(createRover(input[i]));
+    directions.push(input[i+1]);
   }
 };
 
