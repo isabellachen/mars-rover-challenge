cc66e376342ddbf1aacb9014636f6a3d1ab5b565
Isabella Chen <miss.izzyc@gmail.com>
2017-11-11 10:10:49 +0100

    Bugfix: fixed typo in test

--- tests/mission.test.js
+++ tests/mission.test.js
@@ -4,6 +4,6 @@ const processInput = require('../app/process-input');
 const processOutput = require('../app/process-output');
 const createRover = require('../app/rover');
 
-test('moves rovers into final position', () => {
-  expect(runMission('5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM').toBe('1 3 N\n5 1 E'));
+test('test input matches expected output', () => {
+  expect(runMission('5 5\n1 2 N\nLMLMLMLMM\n3 3 E\nMMRMMRMRRM')).toBe('1 3 N\n5 1 E\n');
 });
